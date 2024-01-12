9ab01e0ce0b2d847cf13fad0a73786e33ab148b7
pahardy <hardy_patrick@yahoo.ca>
2024-01-12 09:33:45 -0500

    add information in k8s provider to connect it to an existing EKS cluster

--- terraform.tf
+++ terraform.tf
@@ -12,6 +12,15 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "ca-central-1"
+}
+
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host = data.aws_eks_cluster.existing-eks-cluster.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.existing-eks-cluster.id]
+    command     = "aws"
+  }
 }
